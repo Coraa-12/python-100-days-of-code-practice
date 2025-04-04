f7e8c40f5a23eb68ac575b9e05ada030cd63d372
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2025-04-05 01:40:59 +0700

    Practice 1: Add initial implementation of greeting and username length calculation

---
+++ day_1/practice_1.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+
+print("Hello, World!\nHello, World!")
+
+print("Hello " + "Alice!")
+
+# Ask for the user name before printing the variable into the terminal
+# print("Hello " + input("What's your name? ") + "!")
+
+# print(len(input("What's your name? ")))
+
+username = input("What's your name? ")
+length = len(username)
+print(length)
+
+
+
+
+
