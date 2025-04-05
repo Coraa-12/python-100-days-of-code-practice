51c3775fe677737874b167527d31737872b2dfe4
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2025-04-05 10:21:19 +0700

    EC1.2: Add simple calculator program to prompt user for two numbers and display their sum

---
+++ day-1/extra_challenges_1/challenge_vars_2.py
@@ -0,0 +1,12 @@
+# 05 April 2025
+
+# Simple Calculator:
+
+# 1. Ask the user to input two numbers. Store these numbers as variables.
+# 2. Print the sum of the two numbers.
+# 3. Try to break the code by inputting string instead of number. explain the error.
+
+first_num = int(input("Enter the first num: "))
+second_num = int(input("Enter the second num: "))
+
+print(first_num + second_num)
