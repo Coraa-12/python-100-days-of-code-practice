4eda5dba4f4348f77adedb8469241c1fd3772c5e
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2025-04-06 00:36:30 +0700

    Add playground_2.py with basic Python operations and data types examples

---
+++ day-02/playground_2.py
@@ -0,0 +1,44 @@
+print("Hello"[-1])
+
+print(5 + 3)
+
+# Int
+print(1_225_000)
+
+# Float
+print(12532.790)
+
+# Boolean
+print(True)
+
+# Mathematical Operations
+print(int(6 / 2)) # (DIVISION, CONVERT TO STRING)
+print(5 / 2) # (DIVISION FLOAT DEFAULT)
+print(5 ** 5) # (SQUARE 2)
+
+# PEMDAS manipulation
+print(3 * 3 / 3 + 3 - 3)
+
+# Assignment operator: +=, -=, *=, /=
+score = 2
+
+score += 2
+
+print(score)
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
+
+# Print Different type of data
+print(type("Hello, World!")) # (STR)
+print(type(123)) # (INT)
+print(type(1.42)) # (FLOAT)
+print(type(True)) # (BOOL)
+
+print("Numbers of letters in your name: " + str(len(input("Enter your name: "))))
+
+point = 5
+height = 1.2
+is_winning = True
+
+print(f"Your score is {point}, your height is {height} and you are winning is {is_winning}")
