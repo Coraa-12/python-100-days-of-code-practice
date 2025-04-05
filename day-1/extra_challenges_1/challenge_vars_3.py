f0b969792007d877d5e2e74da1c400470508cbc6
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2025-04-05 10:21:40 +0700

    EC1.3: Add formatted address program to prompt user for address details

---
+++ day-1/extra_challenges_1/challenge_vars_3.py
@@ -0,0 +1,15 @@
+# 05 April 2025
+
+# Formatted Address:
+
+# 1. Ask the user for their street address, city, and postal code.
+# 2. Print the address in a neatly formatted way, with each piece of information on a new line. For example:
+# [Street Address]
+# [City]
+# [Postal Code]
+
+street_address = input("What's your street address? ")
+city = input("What's the city you're currently living in? ")
+postal_code = input("Your postal code? ")
+
+print(street_address + "\n" + city + "\n" + postal_code)
