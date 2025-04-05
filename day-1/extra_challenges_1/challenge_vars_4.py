ddec22f9f3589f44d57a8dc0836cfe695c0e60de
Tegar Wijaya Kusuma <tegarwijayakusuma2004@gmail.com>
2025-04-05 10:21:55 +0700

    EC1.5: Add name and age program to prompt user for input and display age information

---
+++ day-1/extra_challenges_1/challenge_vars_4.py
@@ -0,0 +1,13 @@
+# 05 April 2025
+
+# Name and Age:
+
+# 1. Ask the user for their name and age.
+# 2. Print a statement like "[Name] is [Age] years old."
+# 3. Then, print a new line and add the following statement "Next year, [Name] will be [Age + 1] years old"
+
+name = input("What's your name? ")
+age = int(input("What's your age? "))
+
+print(f"{name} is {age} years old.")
+print(f"Next year, {name} will be {age + 1} years old.")
